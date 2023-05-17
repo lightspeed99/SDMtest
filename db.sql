2f91b183363550a17c09d9066ba973b5bbd6799e
dac <dac@dypm.org>
2023-05-17 12:41:32 +0530

    second push

---
+++ db.sql
@@ -0,0 +1,3 @@
+create table candidate( phone int, email varchar(50), 10th int, 12th int, cllg int,project varchar(50), work experience varchar(50));
+
+create table organisation ( job varchar(50),loaction varchar(50), slaary int, contact int, history job varchar(50))
